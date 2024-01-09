f8f1078e79621316f762d3e12712e564bd7238cf
LenaLih <lena3005@gmail.com>
2024-01-09 15:16:53 +0600

    approp

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,12 +1,9 @@
 // Посчитай и выведи окончательный результат в console.log.
 
-// let x = 5;
-// x += 3;
-// x *= 2;
-// x -= 4;
-// x /= 3;
-   let x = 5;
-   console.log((x += 3));
-   console.log((x *= 2));
-   console.log((x -= 4));
-   console.log((x /= 3));
+let x = 5;
+x += 3;
+x *= 2;
+x -= 4;
+x /= 3;
+
+   console.log(x);
