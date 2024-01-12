f2bc6c290189c1b5d958bc5c414ac2f10df7ce93
LenaLih <lena3005@gmail.com>
2024-01-13 00:58:43 +0600

    arrow function

---
+++ exam.js
@@ -0,0 +1,30 @@
+
+      //   const value = 777;
+      //   if (typeof value === "YYY") {
+      //       const result = value  % 2 === 0 ? "Четное число" : "Нечетное число";
+      //       console.log(result);
+      //   } else {
+      //       console.log("Это вообще не число");
+      //   }
+  
+// ! ЧИТАЙ ВНИМАТЕЛЬНО, НЕ СПЕШИ:
+// Наша программа сломана, необходимо ее починить
+// замени ХХХ, YYY(разумеется вставить в кавычки)  и ZZZ  на что-то более подходящее.
+// Если сделал все правильно, консоль покажет нам правильный ответ.
+// ! Пример: const boolean $  (ЧЕРЕЗ ПРОБЕЛ)
+// ! ОБРАТИ ВНИМАНИЕ: КОВЫЧКИ ИСПОЛЬЗОВАТЬ НЕ НАДО.
+// for (let i = 1; i <= 10; i++) { 
+//    console.log(i); 
+// }
+// let numbers = [1, 2, 3, 4, 5]; // Пример массива чисел 
+// for (let i = 0; i < numbers.length; i++) { 
+//     const number = numbers[i]
+//     console.log(number); 
+// }
+let objects = [{name: 'Alice'}, {name: 'Bob'}, {name: 'Charlie'}]; // Пример массива объектов
+for (let i = 0; i < objects.length; i++) {
+    const object = objects[i]
+    console.log(object.name);
+}
+
+
